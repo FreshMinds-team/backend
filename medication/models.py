c9bb05bceba050f5cbd3c0ebdd6834e95b4f3ed4
SubidShrestha <subideminem@gmail.com>
2022-08-07 02:29:03 +0545

    Medication History of patients

--- medication/models.py
+++ medication/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from user.models import Patient
 
 class Medication(models.Model):
-    patient=models.ForeignKey(Patient,on_delete=models.CASCADE,null=True)
+    patient=models.ForeignKey(Patient,on_delete=models.CASCADE,null=True,related_name='medicine')
     medicine=models.CharField(max_length=255)
     amount=models.CharField(max_length=255)
     description=models.TextField(max_length=255)
--- user/serializers.py
+++ user/serializers.py
@@ -1,10 +1,12 @@
 from xml.etree.ElementInclude import include
 from rest_framework import serializers
 from appointment.serializers import AppointmentSerializer
+from medication.serializers import MedicationSerializer
 from user.models import *
 
 class PatientSerializer(serializers.ModelSerializer):
     appoint_patient = AppointmentSerializer(many=True,read_only=True,allow_null=True)
+    medicine = MedicationSerializer(many=True,read_only=True,allow_null=True)
     class Meta:
         model=Patient
         fields="__all__"
